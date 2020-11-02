c025ec11cbca05ff5cbdb9d2dcfa4c56b281aa3d
LourdesMp <lourdes.merino@imax.com.pe>
2020-11-02 12:46:43 -0500

    json de prueba

---
+++ pytasks/projects.py
@@ -0,0 +1,53 @@
+projects= [
+        {
+            "idProyecto": 1,
+            "nombreProyecto": "Villa Marina",
+            "direccion": "Santa Anita",
+            "promotor": "XXXX",
+            "banco": "BCP"
+        },
+        {
+            "idProyecto": 2,
+            "nombreProyecto": "Villa Marina 2",
+            "direccion": "Santa Anita 2",
+            "promotor": "XXXXX",
+            "banco": "BBVA"
+        },
+        {
+            "idProyecto": 3,
+            "nombreProyecto": "PrimaveraVerde",
+            "direccion": "San Miguel",
+            "promotor": "XXXXX",
+            "banco": "BBVA"
+        },
+        {
+            "idProyecto": 4,
+            "nombreProyecto": "Primavera",
+            "direccion": "Miraflores",
+            "promotor": "XXXXX",
+            "banco": "BCP"
+        },
+        {
+            "idProyecto": 9,
+            "nombreProyecto": "Villa Marina",
+            "direccion": "Santa Anita ",
+            "promotor": "XXXXXXX",
+            "banco": "BCP"
+        },
+        {
+            "idProyecto": 12,
+            "nombreProyecto": "12345",
+            "direccion": "23e4rt",
+            "promotor": "sxdcfvg",
+            "banco": "bcp"
+        },
+        {
+            "idProyecto": 24,
+            "nombreProyecto": "Las Palmeras",
+            "direccion": "Santiago de Surco",
+            "promotor": "xxxxxxxxxxxxx",
+            "banco": "bbva"
+        }
+    ]
+
+
